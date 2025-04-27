3fd802cbfe11e289e103c097bfb2632862ee9a0d
MatheusHMR <matheus.henrimr@gmail.com>
2025-04-27 12:50:12 -0300

    feat: implement the MVP of the project, including saving the best individual and both single and double point crossover

--- src/genetic_algorithm_function_maximizer/GeneticAlgorithm.py
+++ src/genetic_algorithm_function_maximizer/GeneticAlgorithm.py
@@ -1,81 +1,231 @@
 import numpy as np
+import math
+import random
 
 class GeneticAlgorithm:
-    def __init__(self, population_size, chromosome_length, mutation_rate, crossover_rate, elitism_count,
-                 selection_method='roulette', tournament_size=None, crossover_type='one_point'):
+    def __init__(self, population_size, bounds, mutation_rate, crossover_rate, elitism_count = None, max_known_value = None,
+                 selection_method='roulette', tournament_size=None, crossover_type='one_point', decimal_precision=1):
         """
         Inicializa os parâmetros do algoritmo genético.
 
         :param population_size: Tamanho da população.
-        :param chromosome_length: Comprimento do cromossomo (número de genes).
         :param mutation_rate: Taxa de mutação.
         :param crossover_rate: Taxa de cruzamento.
         :param elitism_count: Número de indivíduos a serem selecionados para a próxima geração.
         :param selection_method: Método de seleção (roulette ou tournament).
         :param tournament_size: Tamanho do torneio (se selection_method for tournament).
         :param crossover_type: Tipo de cruzamento (one_point ou two_point).
+        :param max_known_value: Valor máximo conhecido da função, nem sempre é conhecido.
         """
 
         self.population_size = population_size
-        self.chromosome_length = chromosome_length
+        self.bounds = bounds
         self.mutation_rate = mutation_rate
         self.crossover_rate = crossover_rate
         self.elitism_count = elitism_count
         self.selection_method = selection_method
         self.tournament_size = tournament_size
         self.crossover_type = crossover_type
-        self.decimal_precision = 3
-        self.population = self.initialize_population()
-        self.current_population = self.population
-        self.x1_min = -3.1
-        self.x1_max = 12.1
-        self.x2_min = 4.1
-        self.x2_max = 5.8
+        self.max_known_value = max_known_value
+        self.best_individual = None
+        self.best_fitness = None
+        self.current_error = np.inf if max_known_value is not None else None
+        self.mean_error = np.inf if max_known_value is not None else None
+        self.decimal_precision = decimal_precision
+        self.current_population = None
 
-    def real_function(self, population):
+    def real_function(self):
         """
         Função real a ser maximizada.
         """
-        return 21.5 + population[:, 0] * np.sin(4 * np.pi * population[:, 0]) + population[:, 1] * np.sin(20 * np.pi * population[:, 1])
+        return 21.5 + self.current_population[:, 0] * np.sin(4 * np.pi * self.current_population[:, 0]) + self.current_population[:, 1] * np.sin(20 * np.pi * self.current_population[:, 1])
 
     def initialize_population(self):
         """
         Cria a população inicial de indivíduos.
         """
-        bounds = [(self.x1_min, self.x1_max), (self.x2_min, self.x2_max)]  # limites de x1 e x2
 
         # Inicialização
-        population = np.array([
-            [np.round(np.random.uniform(low, high), self.decimal_precision) for (low, high) in bounds]
+        return np.array([
+            [np.round(np.random.uniform(low, high), self.decimal_precision) for (low, high) in self.bounds]
             for _ in range(self.population_size)
         ])
-
-        return population
     
-    def fitness(self, individuals_array):
+    def fitness(self):
         """
-        Calcula a aptidão (fitness) do indivíduo.
+        Calcula a aptidão (fitness) da população.
+        Atualiza o melhor indivíduo, o erro do melhor e o erro médio da população.
         """
-        return self.real_function(individuals_array)
+        fitness_values = self.real_function()
+        # Índice do melhor indivíduo
+        best_idx = np.argmax(fitness_values)
+        self.best_individual = self.current_population[best_idx]
+        self.best_fitness = fitness_values[best_idx]
+
+        if self.max_known_value is not None:
+            # Erro do melhor indivíduo
+            self.current_error = self.max_known_value - self.best_fitness
+            # print(f"Erro do melhor indivíduo: {self.current_error}")
+            # Erro médio da população
+            self.mean_error = np.mean(self.max_known_value - fitness_values)
+            # print(f"Erro médio da população: {self.mean_error}")
+        else:
+            self.current_error = None
+            self.mean_error = None
+
+        return fitness_values
+
+    def get_n_bits(self, precision):
+        """Calcula quantos bits são necessários para cada variável.
+
+        :param precision: Precisão desejada para a conversão.
+
+        :return: Lista com o número de bits necessários para cada variável.
+        """
+        bits = []
+        for low, high in self.bounds:
+            span = int((high - low) * (10**precision))
+            bits.append(math.ceil(math.log2(span + 1)))
+        return bits
+
+    def real_to_bin(self, x, low, n_bits):
+        """Converte valor real x para inteiro com precisão e depois para string binária.
+
+        :param x: Valor real a ser convertido.
+        :param low: Limite inferior da variável.
+        :param n_bits: Número de bits disponíveis para a representação.
+
+        :return: String binária representando o valor real.
+        """
+        factor = 10**self.decimal_precision
+        x_int = int(round((x - low) * factor))
+        return format(x_int, f'0{n_bits}b')
+
+    def bin_to_real(self, bstr, low):
+        """Converte string binária de volta para real, aplicando offset e precisão.
+
+        :param bstr: String binária a ser convertida.
+        :param low: Limite inferior da variável.
+        :param n_bits: Número de bits disponíveis para a representação.
+
+        :return: Valor real convertido.
+        """
+        factor = 10**self.decimal_precision
+        return int(bstr, 2) / factor + low
+    
+    def clip_individual(self, individual):
+        """
+        Clipping do indivíduo para que ele não saia dos limites do problema.
+        """
+
+        # Fazer dessa forma habilita que o crossover ajuste o domínio para o problema independentemente da quantidade de variáveis
+        # lower_bounds = np.array([low for low, _ in self.bounds]) 
+        # upper_bounds = np.array([high for _, high in self.bounds])
+
+        return np.clip(individual, self.bounds[0], self.bounds[1])
+    
+    def single_point_binary_crossover(self, parent1, parent2):
+        """
+        Realiza o cruzamento de ponto único entre dois indivíduos na representação binária.
+        """
+        precision = self.decimal_precision
+
+        bits_list = self.get_n_bits(precision)
+        
+        # 1) Codifica cada indivíduo para representação binária
+        b1 = ''
+        b2 = ''
+        for i in range(parent1.shape[0]):
+            xp1, xp2 = parent1[i], parent2[i]
+            low, _ = self.bounds[i]
+            n_bits = bits_list[i]
+            b1 += self.real_to_bin(xp1, low, n_bits)
+            b2 += self.real_to_bin(xp2, low, n_bits)
+
+        # 2) Escolhe um ponto de corte aleatório
+        point = random.randint(1, len(b1) - 1)
+        
+        # 3) Realiza o cruzamento
+        child_bin = b1[:point] + b2[point:]
+        
+        # 4) Decodifica o filho de volta para valores reais
+        child = []
+        idx = 0 # Começa na posição 0
+        for i, (low, _) in enumerate(self.bounds):
+            n_bits = bits_list[i]
+            snippet = child_bin[idx:idx+n_bits]
+            child.append(self.bin_to_real(snippet, low))
+            idx += n_bits
+
+        return self.clip_individual(np.array(child))
+
+    def double_point_binary_crossover(self, parent1, parent2):
+
+        """
+        Realiza o cruzamento de dois pontos entre dois indivíduos na representação binária.
+        
+        :param p1: Primeiro pai (array de valores reais)
+        :param p2: Segundo pai (array de valores reais)
+        :param bounds: Lista de tuplas com os limites (min, max) de cada variável
+        :param precision: Precisão decimal para a codificação binária
+        :return: Filho resultante do cruzamento (array de valores reais)
+        """
+        # Determina o número de bits necessários para cada variável
+        bits_list = self.get_n_bits(self.decimal_precision)
+        
+        # 1) Codifica cada indivíduo para representação binária
+        b1 = ''
+        b2 = ''
+        for i in range(parent1.shape[0]):
+            xp1, xp2 = parent1[i], parent2[i]
+            low, _ = self.bounds[i]
+            n_bits = bits_list[i]
+            b1 += self.real_to_bin(xp1, low, n_bits)
+            b2 += self.real_to_bin(xp2, low, n_bits)
+    
+        # 2) Escolhe dois pontos de corte aleatórios
+        a, b = sorted(random.sample(range(1, len(b1)), 2)) # Ordena os pontos para que a seja o menor
+        
+        # 3) Realiza o cruzamento
+        child_bin = b1[:a] + b2[a:b] + b1[b:]
+
+        # Exemplo caso a = 2 e b = 5 e len(b1) = 8
+        # b1 = 11100011
+        # b2 = 00111100
+        # child_bin = 11|111|011
+        #             ^^|^^^|^^^
+        #             b1|b2 |b1
+
+        # 4) Decodifica o filho de volta para valores reais
+        child = []
+        idx = 0
+        for i, (low, _) in enumerate(self.bounds):
+            n_bits = bits_list[i]
+            snippet = child_bin[idx:idx+n_bits]
+            child.append(self.bin_to_real(snippet, low))
+            idx += n_bits
+
+        return self.clip_individual(np.array(child))
 
     def selection(self):
         """
         Seleciona os indivíduos para reprodução, com base no método definido.
         """
+        # TODO implementar o elitismo
+
         # Seleciona o método de seleção
         if self.selection_method == 'roulette':
             # Seleciona os indivíduos para reprodução
-            return self.roulette_selection()
+            self.current_population = self.roulette_selection()
         elif self.selection_method == 'tournament':
             # Seleciona os indivíduos para reprodução
-            return self.tournament_selection()
+            self.current_population = self.tournament_selection()
 
-    def roulette_selection(self):
+    def roulette_selection(self, fitness_values):
         """
         Implementa a seleção por roleta.
         """
-        # Calcula a aptidão de cada indivíduo
-        fitness_values = self.fitness(self.current_population)
+
         # Calcula a probabilidade de cada indivíduo
         probabilities = fitness_values / np.sum(fitness_values)
         # Seleciona os indivíduos para reprodução
@@ -83,26 +233,88 @@ class GeneticAlgorithm:
         # Retorna os indivíduos selecionados
         return self.current_population[selected_individuals]
 
-    def tournament_selection(self):
+    def tournament_selection(self, fitness_values):
         """
         Implementa a seleção por torneio.
         """
-        pass
+        raise NotImplementedError("Tournament selection not implemented yet")
 
-    def crossover(self, parent1, parent2):
+    def crossover(self):
         """
         Realiza o cruzamento entre dois pais (one-point ou two-point).
         """
-        pass
+        np.random.shuffle(self.current_population)
+        children = []
+        n = len(self.current_population)
+        i = 0
+        while i < n - 1: # Parar antes do último se for ímpar
+            parent1 = self.current_population[i]
+            parent2 = self.current_population[i+1]
+            if random.random() < self.crossover_rate:
+                if self.crossover_type == 'one_point':
+                    child1 = self.single_point_binary_crossover(parent1, parent2)
+                    child2 = self.single_point_binary_crossover(parent2, parent1)       
+                elif self.crossover_type == 'two_point':
+                    child1 = self.double_point_binary_crossover(parent1, parent2)
+                    child2 = self.double_point_binary_crossover(parent2, parent1)
+                else:
+                    raise ValueError("Invalid crossover type")
+                children.extend([child1, child2])
+            else:
+                children.extend([parent1, parent2])
+            i += 2
+        self.current_population = np.array(children)
 
-    def mutation(self, individual):
+        if n % 2 == 1:
+            children.append(self.current_population[-1])
+        self.current_population = np.array(children)
+
+    def mutation(self):
         """
         Aplica a mutação no indivíduo.
         """
-        pass
+
+        for idx, individual in enumerate(self.current_population):
+            for i in range(len(individual)):
+                if random.random() < self.mutation_rate:
+                    print(f"Mutando o indivíduo {idx} na posição {i}")
+                    print(f"Individual original: {individual}")
+                    binary_individual = list(self.real_to_bin(
+                        individual[i], 
+                        self.bounds[i][0], 
+                        self.get_n_bits(self.decimal_precision)[i]
+                    ))
+                    point = random.randint(0, len(binary_individual) - 1)
+                    binary_individual[point] = '1' if binary_individual[point] == '0' else '0'
+                    binary_individual = ''.join(binary_individual)
+                    individual[i] = self.bin_to_real(binary_individual, self.bounds[i][0])
+                    individual = self.clip_individual(individual)
+                    print(f"Individual mutado: {individual}")
+                    self.current_population[idx] = individual
+        
 
     def run(self, generations):
         """
         Executa o algoritmo genético por um número definido de gerações.
+        
+        :param generations: Número de gerações a serem executadas.
+        :return: O melhor indivíduo encontrado.
         """
-        pass
+        self.current_population = self.initialize_population()
+        for _ in range(generations):
+            print(f"Geração {_ + 1}")
+            
+            # Calcula a aptidão de cada indivíduo, 
+            fitness_values = self.fitness()
+
+            # Faz a seleção, crossover e mutação
+            self.selection(fitness_values)
+            self.crossover() 
+            self.mutation()  
+
+            self.fitness()
+            if self.max_known_value is not None and self.current_error < 1e-6:
+                print(f"Encerrando o algoritmo, pois o erro é menor que 1e-6")
+                break     
+
+        return self.best_individual, self.best_fitness
--- src/genetic_algorithm_function_maximizer/main.py
+++ src/genetic_algorithm_function_maximizer/main.py
@@ -1,5 +1,22 @@
 from GeneticAlgorithm import GeneticAlgorithm
 
 if __name__ == "__main__":
-    ga = GeneticAlgorithm(population_size=100, chromosome_length=2, mutation_rate=0.01, crossover_rate=0.8, elitism_count=10)
-    print(ga.roulette_selection())
+
+    bounds = [(-3.1, 12.1), (4.1, 5.8)]
+    precision = 2
+
+    max_known_value = 38.85
+
+    ga = GeneticAlgorithm(
+        population_size=100, 
+        bounds=bounds, 
+        mutation_rate=0.5, 
+        crossover_rate=0.75, 
+        elitism_count=10,
+        max_known_value=max_known_value
+    )
+
+    best_individual, best_fitness = ga.run(generations=1000)
+    print("best_individual: ", best_individual)
+    print("best_fitness: ", best_fitness)
+
